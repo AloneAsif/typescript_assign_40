f99be5563e2dfcc4054ba133e4cee11e6fea56cb
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-16 09:37:08 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question no 41       Magicians:
+function show_magicians(magician) {
+    for (let magic of magician) {
+        console.log(magic);
+    }
+}
+let magicians = ['Harry Houdini', 'Criss Angel ', 'Penn Jillette'];
+show_magicians(magicians);
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Question no 41       Magicians:
+
+function show_magicians(magician:string[]){
+    for (let magic of magician){
+        console.log(magic)
+    }
+}
+
+let magicians:string[] = ['Harry Houdini','Criss Angel ','Penn Jillette'];
+
+
+show_magicians(magicians)
